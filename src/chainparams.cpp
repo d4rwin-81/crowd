81cca403e356796a969e9cbd73676d5f6656aba6
d4rwin-81 <mobilefu82@gmail.com>
2017-12-31 11:25:54 +0100

    mn

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -119,7 +119,7 @@ public:
         nPoolMaxTransactions = 3;
         //strSporkKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
         //strMasternodePaymentsPubKey = "046f78dcf911fbd61910136f7f0f8d90578f68d0b3ac973b5040fb7afb501b5939f39b108b0569dca71488f5bbf498d92e4d1194f6f941307ffd95f75e76869f0e";
-        strDarksendPoolDummyAddress = "C7FBJNGDmEsU5wx2m3xw85N8kRgCqA8S7L";
+        strDarksendPoolDummyAddress = "CDAFuY5ZutCiYDoVyAWNKkxrCvwerrLxM6";
         nLastPOWBlock = 1024;
         nPOSStartBlock = 1000;
     }
